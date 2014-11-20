673a14731861d440f25ec118832fcfc7e4e3aa62
anaprados <abrprados@gmail.com>
2014-11-20 12:46:42 +0100

    Create the main layer

--- Tandemr/gen/moose/tandemr/R.java
+++ Tandemr/gen/moose/tandemr/R.java
@@ -8,6 +8,9 @@
 package moose.tandemr;
 
 public final class R {
+    public static final class array {
+        public static final int gender_array=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -23,20 +26,32 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int round_button=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int gender_spinner=0x7f090004;
+        public static final int textedit_name=0x7f090002;
+        public static final int textview_gender=0x7f090003;
+        public static final int textview_name=0x7f090001;
+        public static final int welcome_button=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_profile=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+        public static final int profile=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int profile_gender=0x7f050005;
+        public static final int profile_name=0x7f050004;
+        public static final int title_activity_profile=0x7f050006;
+        public static final int welcome_bottom=0x7f050003;
     }
     public static final class style {
         /** 
@@ -60,10 +75,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- Tandemr/src/moose/tandemr/MainActivity.java
+++ Tandemr/src/moose/tandemr/MainActivity.java
@@ -1,9 +1,12 @@
 package moose.tandemr;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +15,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button button = (Button) findViewById(R.id.welcome_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            	Intent intent =  new Intent(MainActivity.this, ProfileActivity.class);
+            	
+            	startActivity(intent);
+            }
+        });
     }
 
 
