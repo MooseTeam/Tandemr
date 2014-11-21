d45873b601de1c5b986edef68b59ba270ed5e704
Bénédicte Lagouge <lagouge@polytech.unice.fr>
2014-11-21 16:29:41 +0100

    creating the activity that will show the other users' profiles . Adding an image of a cute pinguin into drawable, so I can put it as the example image for the profile of another user (later obviously we won't need it anymore)

--- Tandemr/gen/moose/tandemr/R.java
+++ Tandemr/gen/moose/tandemr/R.java
@@ -28,38 +28,46 @@ public final class R {
         public static final int btn_search=0x7f020000;
         public static final int ic_action_folder_open=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int round_button=0x7f020003;
+        public static final int moose=0x7f020003;
+        public static final int pingui=0x7f020004;
+        public static final int round_button=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
-        public static final int btn_search=0x7f090004;
+        public static final int action_settings=0x7f09000a;
+        public static final int btn_done=0x7f090009;
+        public static final int btn_search=0x7f090005;
         public static final int gender_spinner=0x7f090002;
+        public static final int imageViewCircular=0x7f090004;
         public static final int textedit_birthdate=0x7f090003;
-        public static final int textedit_email=0x7f090006;
-        public static final int textedit_message=0x7f090005;
+        public static final int textedit_email=0x7f090007;
+        public static final int textedit_message=0x7f090006;
         public static final int textedit_name=0x7f090001;
-        public static final int textedit_phone=0x7f090007;
+        public static final int textedit_phone=0x7f090008;
         public static final int welcome_button=0x7f090000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_profile=0x7f030001;
+        public static final int activity_foreign_profile=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_profile=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int profile=0x7f080001;
+        public static final int foreign_profile=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int profile=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int profile_birthdate=0x7f050006;
+        public static final int profile_done=0x7f05000d;
         public static final int profile_email=0x7f050009;
         public static final int profile_gender=0x7f050005;
         public static final int profile_message=0x7f050008;
         public static final int profile_name=0x7f050004;
         public static final int profile_phone=0x7f05000a;
         public static final int profile_search=0x7f050007;
+        public static final int title_activity_foreign_profile=0x7f05000c;
         public static final int title_activity_profile=0x7f05000b;
         public static final int welcome_bottom=0x7f050003;
     }
