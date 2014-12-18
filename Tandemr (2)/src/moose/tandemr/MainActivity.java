463f003bebb7a4c2f492249381a4ab5bcb41de89
Sergikito <sergikito@gmail.com>
2014-12-18 22:42:54 +0100

    MainActivity clearer

--- Tandemr (2)/src/moose/tandemr/MainActivity.java
+++ Tandemr (2)/src/moose/tandemr/MainActivity.java
@@ -17,13 +17,12 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
@@ -31,40 +30,27 @@ public class MainActivity extends ActionBarActivity {
 	private String[] optionsMenu;
 	private DrawerLayout drawerLayout;
 	private ListView drawerList;
-	private CharSequence tituloSeccion= null;
-	private CharSequence tituloApp;
+	private CharSequence sectionTittle= null;
+	private CharSequence appTittle;
 	private ActionBarDrawerToggle drawerToggle;
 
-	private BluetoothAdapter mBluetoothAdapter = null;
-	private static final boolean D = true;
+	private BluetoothAdapter BluetoothAdapter = null;
 
 	// Intent request codes
 	private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
 	private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
 	private static final int REQUEST_ENABLE_BT = 3;
-
-	public boolean isDrawerOpen(int gravity) {
-		return drawerLayout != null && drawerLayout.isDrawerOpen(gravity);
-	}
-
-	public void closeDrawer(int gravity) {
-		drawerLayout.closeDrawer(gravity);
-	}
-
-	public void openDrawer(int gravity) {
-		drawerLayout.openDrawer(gravity);
-	}
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		// Get local Bluetooth adapter
-		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		BluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
 		// If the adapter is null, then Bluetooth is not supported
-		if (mBluetoothAdapter == null) {
+		if (BluetoothAdapter == null) {
 			Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
 			finish();
 			return;
@@ -72,29 +58,35 @@ public class MainActivity extends ActionBarActivity {
 
 		// If BT is not on, request that it be enabled.
 		// setupChat() will then be called during onActivityResult
-		if (!mBluetoothAdapter.isEnabled()) {
+		if (!BluetoothAdapter.isEnabled()) {
 			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
 
 		}
-
+		
+		//get from the xmls the elements which we need
 		optionsMenu = getResources().getStringArray(R.array.nav_drawer_items);
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		drawerList = (ListView) findViewById(R.id.right_drawer);
 
+		//fill the NavDrawer with the required information.
 		drawerList.setAdapter(new ArrayAdapter<String>(getSupportActionBar().getThemedContext(),
 				(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) ?
 						android.R.layout.simple_list_item_activated_1 :
 							android.R.layout.simple_list_item_1, optionsMenu));//http://www.sgoliver.net/blog/?p=4104
 
+		
+		//when the app is launched, it shows the fragment FindPeople by default.
 		Fragment fragment= new FindPeople();
 		FragmentManager fragmentManager = getSupportFragmentManager();
 		fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
-		tituloSeccion=optionsMenu[0];
-		getSupportActionBar().setTitle(tituloSeccion);
+		sectionTittle=optionsMenu[0];
+		getSupportActionBar().setTitle(sectionTittle);
 		drawerList.setItemChecked(0, true);
 
-
+		
+		//make the onclicklistener of the elements in the NavDrawer and shows, detects what is the element
+		//used, and changes the view of the Fragments to see the needed one.
 		drawerList.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView parent, View view,
@@ -126,40 +118,33 @@ public class MainActivity extends ActionBarActivity {
 
 				drawerList.setItemChecked(position, true);
 
-				/*CharSequence*/ tituloSeccion = optionsMenu[position];
-				getSupportActionBar().setTitle(tituloSeccion);
+				sectionTittle = optionsMenu[position];
+				getSupportActionBar().setTitle(sectionTittle);
 
 				drawerLayout.closeDrawer(drawerList);
 			}
 		});
 
-		/*CharSequence */tituloApp = getTitle();
+		appTittle = getTitle();
 
-
-		/*ActionBarDrawerToggle*/ drawerToggle = new ActionBarDrawerToggle(this,
+		//ActionBarDrawerToggle provides a handy way to tie together the functionality of DrawerLayout
+		//and the framework ActionBar to implement the recommended design for navigation drawers.
+		drawerToggle = new ActionBarDrawerToggle(this,
 				drawerLayout,
-				R.drawable.ic_drawer,
 				R.string.drawer_open,
 				R.string.drawer_close) {
-
+			
+			//Instructions when the NavDrawer is closed and when it is open
 			public void onDrawerClosed(View view) {
-				getSupportActionBar().setTitle(tituloSeccion);
+				getSupportActionBar().setTitle(sectionTittle);
 				ActivityCompat.invalidateOptionsMenu(MainActivity.this);
 			}
 
 			public void onDrawerOpened(View drawerView) {
-				getSupportActionBar().setTitle(tituloApp);
+				getSupportActionBar().setTitle(appTittle);
 				ActivityCompat.invalidateOptionsMenu(MainActivity.this);
 			}
 		};
-
-		drawerLayout.setDrawerListener(drawerToggle);
-
-		/*getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		getSupportActionBar().setHomeButtonEnabled(true);
-		 */
-
-
 	}
 
 	/**
@@ -200,10 +185,8 @@ public class MainActivity extends ActionBarActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-
+		//create an aditional button to substitute the original NavigationDrawerToggle
+		//and gives it the same utility. if the NavDrawer is open, close it, and if not, open it.
 		if (item != null && item.getItemId() == R.id.action_compose) {
 			if (drawerLayout.isDrawerOpen(Gravity.RIGHT)) {
 				drawerLayout.closeDrawer(Gravity.RIGHT);
@@ -212,46 +195,38 @@ public class MainActivity extends ActionBarActivity {
 			}
 			return true;
 		}    	
-
-		if (drawerToggle.onOptionsItemSelected(item)) {
-			return true;
-		}
-
-		/*int id = item.getItemId();
-	        if (id == R.id.action_compose) {
-	        	drawerLayout.openDrawer(Gravity.RIGHT);
-	            return true;
-	        }*/
-
 		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
+		//synchronize the navdrawer with the drawerlayout
 		drawerToggle.syncState();
 	}
-
+	
+	
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 		drawerToggle.onConfigurationChanged(newConfig);
 	}
-
+	
+	//Changing Fragment using buttons instead of the navdrawer
 	public void selectFrag(View view) {
 		Fragment fr=null;
 
 		if(view == findViewById(R.id.welcome_button)){
 			fr = new ProfileActivity();
-			tituloSeccion=optionsMenu[1];
-			getSupportActionBar().setTitle(tituloSeccion);
+			sectionTittle=optionsMenu[1];
+			getSupportActionBar().setTitle(sectionTittle);
 			drawerList.setItemChecked(1, true);
 		}
 
 		else if(view == findViewById(R.id.btn_done)) {
 			fr = new AroundYou();
-			tituloSeccion=optionsMenu[2];
-			getSupportActionBar().setTitle(tituloSeccion);
+			sectionTittle=optionsMenu[2];
+			getSupportActionBar().setTitle(sectionTittle);
 			drawerList.setItemChecked(2, true);
 		}
 
@@ -262,5 +237,5 @@ public class MainActivity extends ActionBarActivity {
 		fragmentTransaction.addToBackStack(null);
 		fragmentTransaction.commit();
 	}
-	
+
 }
